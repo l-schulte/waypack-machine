4f22a2902ec306730adaed167946e5d2116346a3
Lukas <lukas.schulte@uni-passau.de>
2025-10-19 15:01:18 +0200

    refactor handle_request_with_api to improve subpath handling and simplify package name extraction

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def handle_npm_request(timestamp, subpath):
 def handle_yarn_request(timestamp, subpath):
     return handle_request_with_api(yarn_api, timestamp, subpath)
 
-
-def handle_request_with_api(api: NpmCompatibleAPI, timestamp, subpath):
-    if len(subpath.split("/")) > 1:
+ 
+def handle_request_with_api(api: NpmCompatibleAPI, timestamp, subpath: str):
+    if (subpath[0] == "@" and len(subpath.split("/")) > 2 or subpath[0] != "@" and len(subpath.split("/")) > 1):
         return redirect(f"{api.registry_url}{subpath}", code=302)
 
     # Convert timestamp to integer
@@ -34,7 +34,7 @@ def handle_request_with_api(api: NpmCompatibleAPI, timestamp, subpath):
         return "Invalid timestamp format", 400
 
     # Fetch package metadata
-    package_name = subpath.split("/")[0]
+    package_name = subpath
     package_response = api.fetch_package_metadata(package_name)
     if package_response.status_code != 200:
         return (
