259696bdfe43ac5110183f859b0818dd540b61cc
Lukas <l.schulte@mailbox.org>
2025-10-29 17:13:13 +0100

    adds custom cache handling for external resources with local fallback

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, Response, redirect, send_from_directory
 import logging
 from datetime import datetime, timezone
 from endpoints.npm_compatible_apis import NpmCompatibleAPI
 import json
 import os
+import requests
+import hashlib
 
 
 app = Flask(__name__)
@@ -84,6 +86,46 @@ def serve_local_file(subpath):
         return send_from_directory("local_files", subpath)
     except FileNotFoundError:
         return f"Local file not found: {subpath}", 404
+
+
+@app.route("/custom_cache/<path:baseurl>/resource/<path:subpath>")
+def handle_custom_cache(baseurl, subpath):
+    """
+    Handle custom cache requests.
+
+    Route provides the original URL. Function checks if the requested resource
+    (subpath) is available as a local file and serves it. If not found, it gets
+    the resource from the original URL (baseurl/subpath), stores it, and serves it.
+
+    Example: https://www.electronjs.org/headers/v13.1.8/node-v13.1.8-headers.tar.gz
+    Baseurl: https://www.electronjs.org/headers
+    Subpath: v13.1.8/node-v13.1.8-headers.tar.gz
+
+    Example request:
+    GET /custom_cache/https://www.electronjs.org/headers/resource/v13.1.8/node-v13.1.8-headers.tar.gz
+    """
+    full_url = f"{baseurl}/{subpath}"
+    cache_path = f"{hashlib.sha256(baseurl.encode()).hexdigest()}/{subpath}"
+
+    if os.path.exists(f"./local_files/{cache_path}"):
+        return serve_local_file(cache_path)
+
+    print(f"Fetching resource from: {full_url}")
+    response = requests.get(full_url, stream=True)
+    print(f"Response status code: {response.status_code}")
+    if response.status_code == 200:
+
+        os.makedirs(os.path.dirname(f"./local_files/{cache_path}"), exist_ok=True)
+        with open(f"./local_files/{cache_path}", "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+        return Response(
+            response.iter_content(chunk_size=8192),
+            content_type=response.headers.get("Content-Type"),
+        )
+    else:
+        return f"Resource not found at {full_url}", response.status_code
 
 
 def handle_request_with_api(api: NpmCompatibleAPI, timestamp, subpath: str):
